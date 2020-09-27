14329a52df51346b940bdbb55315bc5f5b1adf8d
vagrant <rik.s.1005@gmail.com>
2020-09-27 14:06:34 +0000

    ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,36 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :end_users
-  get 'posts/index'
-  get 'posts/show'
-  get 'posts/new'
-  get 'posts/edit'
-  get 'home/top'
-  get 'home/about'
+
+  devise_for :admins, controllers: {
+    sessions: 'admins/sessions'
+  }
+
+  namespace :admins do
+    get 'top' => 'home#top', as: 'top'
+    post 'admins/posts/check' => 'posts#check', as: 'posts_check'
+    post 'admins/post_comments/check' => 'post_comments#check', as: 'post_comments_check'
+    resources :health_courses
+    resources :end_users, only: [:index, :show]
+    resources :posts, only: [:index, :show, :destroy] do
+      resources :post_comments, only: [:destroy]
+    end
+  end
+
+  devise_for :end_users, controllers: {
+    sessions: 'end_users/sessions',
+    registrations: 'end_users/registrations',
+    passwords: 'end_users/passwords'
+  }
+
+  scope module: :end_user do
+    root 'home#top'
+    get 'about' => 'home#about'
+    resources :posts do
+      resources :post_comments, only: [:create, :destroy]
+      resource :likes, only: [:create, :destroy]
+    end
+    resources :end_users, only: [:show, :edit, :update]
+  end
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ test/controllers/end_users/posts_controller_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class EndUsers::PostsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get end_users_posts_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get end_users_posts_show_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get end_users_posts_new_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get end_users_posts_edit_url
+    assert_response :success
+  end
+
+end
