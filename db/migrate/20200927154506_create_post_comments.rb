a35336e42bc93604364999a4626ea31c874a2301
vagrant <rik.s.1005@gmail.com>
2020-09-29 12:30:23 +0000

    [Update]migrate

--- db/migrate/20200927154506_create_post_comments.rb
+++ db/migrate/20200927154506_create_post_comments.rb
@@ -1,8 +1,8 @@
 class CreatePostComments < ActiveRecord::Migration[5.2]
   def change
     create_table :post_comments do |t|
-      t.references :end_user, null: false, foreign_key: true
-      t.references :post, null: false, foreign_key: true
+      t.integer :end_user_id, null: false, foreign_key: true
+      t.integer :post_id, null: false, foreign_key: true
       t.text :comment, null: false
       t.boolean :mark, null: false, default: false
       t.timestamps
