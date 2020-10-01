86edbe286c7d2bd97ce178a02809bf6346bb97f6
vagrant <rik.s.1005@gmail.com>
2020-10-01 14:19:56 +0000

    [Add]end_userモデルにバリデーション追加

--- app/models/end_user.rb
+++ app/models/end_user.rb
@@ -7,4 +7,10 @@ class EndUser < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :post_comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+
+  validates :name, presence: true, uniqueness: true,length: {in: 1..20}
+  validates :birthday, presence: true
+  validates :postal_code, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :encrypted_password, presence: true
 end
