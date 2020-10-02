53abb6aedbb2ff5ab071ce15284f9a1497fa26e2
vagrant <rik.s.1005@gmail.com>
2020-10-02 15:33:23 +0000

    [Add]posts/new.html

--- app/controllers/end_users/posts_controller.rb
+++ app/controllers/end_users/posts_controller.rb
@@ -1,9 +1,15 @@
 class EndUsers::PostsController < ApplicationController
+
   def index; end
 
   def show; end
 
-  def new; end
+  def new
+    @post = Post.new
+    @departments = Department.all
+    @genres = Genre.all
+  end
 
   def edit; end
+
 end
