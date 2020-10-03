3a95ee42b86c9b0ab9ca98fa3c36c12f1cf893d9
vagrant <rik.s.1005@gmail.com>
2020-10-03 21:23:49 +0000

    [Update]post_commentアクション、ビュー作成

--- app/controllers/end_users/post_comments_controller.rb
+++ app/controllers/end_users/post_comments_controller.rb
@@ -1,2 +1,25 @@
 class EndUsers::PostCommentsController < ApplicationController
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = current_end_user.post_comments.new(post_comment_params)
+    @comment.post_id = @post.id
+    if @comment.save
+      redirect_to post_path(@post)
+    else
+      render 'posts/show'
+    end
+  end
+
+  def destroy
+    PostComment.find_by(id: params[:id], post_id: params[:post_id]).destroy
+    redirect_to post_path(params[:post_id])
+  end
+
+  private
+
+  def post_comment_params
+    params.require(:post_comment).permit(:comment)
+  end
+
 end
