b87efa1614d53e9b0e50b3c08247ee62071175e6
vagrant <rik.s.1005@gmail.com>
2020-10-05 18:41:00 +0000

    [add]end_user側の講座情報機能実装

--- app/controllers/end_users/health_courses_controller.rb
+++ app/controllers/end_users/health_courses_controller.rb
@@ -1,7 +1,11 @@
 class EndUsers::HealthCoursesController < ApplicationController
+
   def index
+    @health_courses = HealthCourse.all.order(id: "DESC")
   end
 
   def show
+    @health_course = HealthCourse.find(params[:id])
   end
+
 end
