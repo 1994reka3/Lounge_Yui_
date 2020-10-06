8c6a671481357cbd3f0ecf48b05d9d699baf72c0
vagrant <rik.s.1005@gmail.com>
2020-10-06 08:14:54 +0000

    [update]end_user/show,edit機èページ作成

--- app/controllers/end_users/end_users_controller.rb
+++ app/controllers/end_users/end_users_controller.rb
@@ -1,13 +1,30 @@
 class EndUsers::EndUsersController < ApplicationController
   before_action :authenticate_end_user!
+  before_action :ensure_end_user
 
   def show
-    @end_user = EndUser.find(params[:id])
     @posts = @end_user.posts
   end
 
   def edit
-    @end_user = EndUser.find(params[:id])
   end
 
+  def update
+    if @end_user.update(end_user_params)
+      flash[:success] = "内容を変更しました"
+      redirect_to end_user_path(@end_user)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def end_user_params
+    params.require(:end_user).permit(:name, :birthday, :postal_code, :email, :image)
+  end
+
+  def ensure_end_user
+    @end_user = EndUser.find(params[:id])
+  end
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'b2f20e6c8f3a071c31a929b1ac9924900ef5c963dc690d854572e4696dec5caaf98e46db1586e06b290699d50a8295d39ad541253a731332ead83e007e884a6d'
