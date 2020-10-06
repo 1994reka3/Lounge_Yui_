f98cf7050117ba4190983edc8dd8479c56019210
vagrant <rik.s.1005@gmail.com>
2020-10-06 07:11:41 +0000

    [Add]end_user/showページ作成、gem'refile'追加

--- app/controllers/end_users/end_users_controller.rb
+++ app/controllers/end_users/end_users_controller.rb
@@ -1,6 +1,13 @@
 class EndUsers::EndUsersController < ApplicationController
   before_action :authenticate_end_user!
-  def show; end
 
-  def edit; end
+  def show
+    @end_user = EndUser.find(params[:id])
+    @posts = @end_user.posts
+  end
+
+  def edit
+    @end_user = EndUser.find(params[:id])
+  end
+
 end
