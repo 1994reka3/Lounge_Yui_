40edd6815fb253e36ec82f2b8ddb2bd0ade1e84e
vagrant <rik.s.1005@gmail.com>
2020-10-09 10:11:12 +0000

    [update]fontfamily設定、post,health_couseのレイアウト修正

--- app/controllers/end_users/health_courses_controller.rb
+++ app/controllers/end_users/health_courses_controller.rb
@@ -2,7 +2,7 @@ class EndUsers::HealthCoursesController < ApplicationController
 
   def index
     if params[:location]
-      @health_courses = HealthCourse.where(location: params[:location])
+      @health_courses = HealthCourse.where(location: params[:location]).page(params[:page]).per(10)
     else
       @health_courses = HealthCourse.order(date: "DESC").page(params[:page]).per(10)
     end
