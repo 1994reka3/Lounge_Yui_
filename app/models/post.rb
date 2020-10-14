e334c6784dfa70c403025e85619bc8a8985cab4c
vagrant <rik.s.1005@gmail.com>
2020-10-14 06:22:27 +0000

    [update]post/indexページに検索ワードを表示ã

--- app/models/post.rb
+++ app/models/post.rb
@@ -12,10 +12,11 @@ class Post < ApplicationRecord
     likes.where(end_user_id: end_user.id).exists?
   end
 
-  #投稿IDの降順に並び替え
+  # 投稿IDの降順に並び替え
   scope :desc_list, -> { order(id: "DESC")}
-  #診療科検索
-  scope :department_search, -> { where(department_id: "#{name}") }
-  #診療科検索後、投稿ID順に並び替え
-  scope :department_desc, -> { department_search.desc_list }
+  # 診療科検索
+  scope :department_search, -> name { where(department_id: name) }
+  # ジャンル検索
+  scope :genre_search, -> genre_id { where(genre_id: genre_id) }
+
 end
