213a2e9728ff28dfd8a093938ff98bceb2ab3baf
vagrant <rik.s.1005@gmail.com>
2020-11-12 12:24:27 +0000

    [update]@post.saveのモデルテスト記述

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,1 +1,18 @@
 require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  context "データが正しく保存される" do
+    before do
+      @post = Post.new
+      @post.end_user_id = 1
+      @post.department_id = 1
+      @post.genre_id = 1
+      @post.title = "テストタイトル"
+      @post.contents = "テスト本文"
+      @post.save
+    end
+    it "全て入力してあるので保存される" do
+      expect(@post).to be_valid
+    end
+  end
+end
