09a6edd9999b8985b99f1929a67c627f008aff89
vagrant <rik.s.1005@gmail.com>
2020-11-13 14:35:54 +0000

    [update]factories/end_users.rbにテスト用end_user情報記述

--- spec/factories/end_users.rb
+++ spec/factories/end_users.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :end_user do
-    
+    email: "test@test"
+    encrypted_password: "000000"
+    name: "test"
+    birthday: "2000-01-01"
+    postal_code: "1111111"
   end
 end
