860a95da387d1212d398b013e32482d7dab5b199
vagrant <rik.s.1005@gmail.com>
2020-11-14 15:07:05 +0000

    [update]postモデルの@post.end_user_idの記述を変更

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,18 +1,19 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe "投稿", type: :model do
-#   context "データが正しく保存される" do
-#     before do
-#       @post = Post.new
-#       @post.end_user_id = "1"
-#       @post.department_id = "1"
-#       @post.genre_id = "1"
-#       @post.title = "テストタイトル"
-#       @post.contents = "テスト本文"
-#       @post.save
-#     end
-#     it "全て入力してあるので保存される" do
-#       expect(@post).to be_valid
-#     end
-#   end
-# end
+RSpec.describe "投稿", type: :model do
+  context "データが正しく保存される" do
+    before do
+      @end_user = FactoryBot.create(:end_user)
+      @post = Post.new
+      @post.end_user_id = @end_user_id
+      @post.department_id = "1"
+      @post.genre_id = "1"
+      @post.title = "テストタイトル"
+      @post.contents = "テスト本文"
+      @post.save
+    end
+    it "全て入力してあるので保存される" do
+      expect(@post).to be_valid
+    end
+  end
+end
